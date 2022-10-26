9ec8459253fc164a490b7722112386b7e446d79b
Edgar Luque <git@edgarluque.com>
2022-10-26 11:14:48 +0200

    improve docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+//!
+//! [![Version](https://img.shields.io/crates/v/rustyman)](https://crates.io/crates/rustyman)
+//! [![Downloads](https://img.shields.io/crates/d/rustyman)](https://crates.io/crates/rustyman)
+//! [![License](https://img.shields.io/crates/l/rustyman)](https://crates.io/crates/rustyman)
+//! ![Rust](https://github.com/edg-l/rustyman/workflows/Rust/badge.svg)
+//! [![Docs](https://docs.rs/rustyman/badge.svg)](https://docs.rs/rustyman)
+//!
+//! Huffman compression and decompression implemented in rust
+//!
+//! ## Example
+//!
+//! ```rust
+//! use rustyman::Huffman;
+//!
+//! let payload = b"hello from the other side of the river";
+//!     
+//! let huffman = Huffman::new_from_data(payload);
+//! let compressed = huffman.compress(payload);
+//! let decompressed = huffman.decompress(&compressed);
+//! 
+//! assert!(compressed.len() < payload.len());
+//! assert_eq!(&payload[..], decompressed);
+//! ```
+
 use std::{
     cell::RefCell,
     collections::{BinaryHeap, HashMap},
